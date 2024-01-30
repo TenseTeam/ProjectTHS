faf3a9c15d9145558e3db64dde218b82b883795d
zVoyder <zvoyder@protonmail.com>
2024-01-30 14:06:45 +0100

    Initial commit

---
+++ Source/ProjectTHS/ProjectTHSGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectTHSGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectTHSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectTHSGameMode();
+};
+
+
+
